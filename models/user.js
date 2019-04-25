24b08a863ec1943439a4b4588ec2800c4cb41f87
Tamara Dragovic <Tamara.Dragovic@eng.it>
2019-04-25 17:40:57 +0200

    add validator

--- models/user.js
+++ models/user.js
@@ -1,18 +1,53 @@
 const mongoose = require('mongoose');
+var crypto = require('crypto');
+var jwt = require('jsonwebtoken');
+
 const Schema = mongoose.Schema;
+
+var validatePassword = password => {
+    if(validator.isLength(password, 22)){
+        logger.info('Password is correct!')
+        return true;
+
+    }else{
+        logger.error('Password is incorrect!')
+        return false;
+    }  
+}
+var validateEmail = email => {
+    if(validator.isEmail(email)){
+        logger.info('Email is correct!')
+        return true;
+    }else{
+        logger.error('Email is incorrect!')
+        return false;
+    }
+}
 
 const UserSchema = new Schema({
     
     name: {
-        type: String
+        type: String,
+        required: true,
+        unique: true
     },
     password: {
-        type: String
+        type: String,
+        default: '',
+        validate: [validatePassword, 'Password is too short']
     },
     email: {
+        type: String,
+        unique: true,
+        required: true,
+        validate: [validateEmail, 'Fill valid email address']
+    },
+    salt: {
         type: String
     }
-
 });
+
+
+
 const User = mongoose.model('user', UserSchema);
 module.exports = User;
