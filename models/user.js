058a0faea959117f46feee1c8c2992cbd7b544dd
Aleksandar Stojmenovic <aleksandar.stojmenovic9@gmail.com>
2019-04-25 16:55:15 +0200

    Added validation for username isLowerCase

--- models/user.js
+++ models/user.js
@@ -1,17 +1,26 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
 
 const Schema = mongoose.Schema;
+
+var validateUsername = function (username) {
+    return validator.isLowercase(username);
+}
 
 const UserSchema = new Schema({
     username: {
         type: String,
-        required: [true, 'Name field is required']
+        required: true,
+        minlength: 5,
+        validate: [validateUsername, 'Username did not pass validation']
+
     },
     password: {
         type: String
     },
 
 })
+
 
 const User = mongoose.model('user', UserSchema);
 module.exports = User;
